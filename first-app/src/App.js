a46de05d0ed00ed30be85a83b5057b6972797fb2
Jason Ng <1995jasonng@gmail.com>
2021-12-13 16:51:05 +1030

    first react app

---
+++ first-app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  // if in regular javascript
+  // const para = document.createElement('p');
+  // para.textContent = 'This is also visible';
+  // document.getElementById('root').append(para);
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible</p>
+    </div>
+  );
+}
+
+export default App;
