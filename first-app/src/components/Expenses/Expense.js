013bf429080d783fbba6dfbd648afe770f7a5b51
Jason Ng <1995jasonng@gmail.com>
2022-02-04 13:01:19 +1030

    filter by year

--- first-app/src/components/Expenses/Expense.js
+++ first-app/src/components/Expenses/Expense.js
@@ -4,28 +4,46 @@ import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import React, { useState } from 'react';
 
+/* For condition check, apart from ? : syntax, && can be used to
+  returns the part after the AND operator if the first condition is met 
+  e.g. i > 0 && a = 1 */ 
+
 const Expense = (props) => {
   // 2020 as default year
-  const [selectedYear, setYear] = useState("2020");
+  const [selectedYear, setYear] = useState("2022");
 
   const saveSelectedYear = (year) => {
     setYear(year);
+  }
+
+  const yearFilteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  }); 
+
+  let expensesContent = <p>No expense found.</p>;
+
+  if (yearFilteredExpenses.length > 0)
+  {
+    expensesContent = yearFilteredExpenses.map((expense) => (
+      <ExpenseItem
+        // should always add a key when mapping out a list, so React can uniquely identify all items and aware which item needed to be replaced
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
   }
 
   return (
     <div>
       <Card className="expenses">
         <ExpenseFilter selected={selectedYear} onSaveSelectedYear={saveSelectedYear}/>
-        {props.items.map((expense) => <ExpenseItem
-          // should always add a key when mapping out a list, so React can uniquely identify all items and aware which item needed to be replaced
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />)}
+          {expensesContent}
       </Card>
     </div> 
   );
 }
 
 export default Expense;
+
